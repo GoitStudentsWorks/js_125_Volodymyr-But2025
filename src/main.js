a5316797332e1681752679a69b6c59549b4f27d9
VovoH <voxa23ne@gmail.com>
2026-04-05 09:35:01 +0300

    js structure

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import Accordion from 'accordion-js';
+import { initcategories } from './js/category';
+import { initAccordion } from './js/modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+
+  // Викликаємо ініціалізацію акордеона
+  initAccordion();
+  initcategories();
+  // Сюди ж потім додаси інші модулі:
+});
